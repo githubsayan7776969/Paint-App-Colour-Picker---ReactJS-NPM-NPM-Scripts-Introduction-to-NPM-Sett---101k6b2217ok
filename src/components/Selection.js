7e9590ddddce13121d95825f58b5ec6d7b4a2f1b
githubsayan7776969 <sayan8910490@gmail.com>
2022-11-17 18:43:08 +0530

    commit one
    commit one

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
 
+import React, { useRef } from "react";
+
+const Selection = (props)=> {
+    const ref = useRef();
+    const handleClick = (e)=> {
+        // console.log(ref.current);
+        props.applyColor(updateSelectionStyle)
+    }
+    const updateSelectionStyle = (colorObj)=> {
+        // console.log(colorObj,ref.current);
+        ref.current.style.background = colorObj.background;
+    }
+    return <div className="fix-box" style={{background:''}} ref = {ref} onClick = {handleClick}>
+        <h2 className="subheading">Selection</h2>
+    </div>
+}
+
+export default Selection;
